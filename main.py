6a17b4efd866ce44f861347781c5a20ee4f36e95
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-13 23:26:17 -0700

    Update main.py

--- main.py
+++ main.py
@@ -25,3 +25,9 @@ def basic_op(operator, value1, value2):
         print('Sorry, this is an incorrect entry')
         res = None
     return res
+
+#-------------------
+#someone else's more effeciant way
+
+def basic_op(operator, value1, value2):
+    return eval(str(value1) + operator + str(value2))
