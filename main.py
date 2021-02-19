f85ee468822796aac5fd5f9d173f4d8150e1cee1
Igor the roofCoder <53712180+russian-droid@users.noreply.github.com>
2021-02-18 22:44:10 -0700

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
-""" 
+"""
+CodeWars kata codewars.com
+
 Your task is to create a function that does four basic mathematical operations.
 The function should take three arguments - operation(string/char), value1(number),
 value2(number).
